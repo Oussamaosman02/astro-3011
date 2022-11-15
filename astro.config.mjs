472f86d2fc88f851985c994338f91a369968ce11
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2022-11-15 13:47:58 +0100

    Updating for vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Oussamaosman02.github.io",
+  output: "server",
+  adapter: vercel()
 });
